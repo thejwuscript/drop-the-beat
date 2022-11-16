0b660fa80aebc3a9d5d57b8984aa40d5cf7b50d3
James Wu <ichangjwu@outlook.com>
2022-11-16 20:53:29 +0900

    import wav file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import './App.css';
+import soundfile from "./assets/wav/drum snare.wav";
 
 function App() {
+  // const audioContext = new AudioContext();
+  // const audioElement = document.querySelector("audio")!;
+  // const track = audioContext.createMediaElementSource(audioElement);
+
   return (
     <div className="App">
+      <audio controls src={soundfile}></audio>
+      <button data-playing="false" role="switch" aria-checked="false">
+        <span>Play/Pause</span>
+      </button>
     </div>
   );
 }
