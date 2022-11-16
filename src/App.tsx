472beec79ac1515a773d7efbf2d3f05927cea520
James Wu <ichangjwu@outlook.com>
2022-11-17 00:07:23 +0900

    Hook up play button

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import soundfile from "./assets/wav/drum snare.wav";
 
 function App() {
-  // const audioContext = new AudioContext();
-  // const audioElement = document.querySelector("audio")!;
-  // const track = audioContext.createMediaElementSource(audioElement);
+  //const [audioBuffer, setAudioBuffer] = useState<AudioBuffer | null>(null);
+  const [audioContext, setAudioContext] = useState<AudioContext | null>(null);
+  const [isPlaying, setIsPlaying] = useState<Boolean>(false);
+
+  useEffect(() => {
+    if (audioContext) {
+      const audio = new Audio(soundfile);
+      fetch(audio.src)
+        .then((res) => res.arrayBuffer())
+        .then((buffer) => audioContext.decodeAudioData(buffer))
+        .then((buffer) => {
+          const track = audioContext.createBufferSource();
+          track.buffer = buffer;
+          track.connect(audioContext.destination);
+          track.start(0);
+        });
+    }
+  }, [audioContext]);
+
+  const handlePlayClick = () => {
+    if (!audioContext) setAudioContext(new AudioContext());
+  };
 
   return (
     <div className="App">
-      <audio controls src={soundfile}></audio>
-      <button data-playing="false" role="switch" aria-checked="false">
-        <span>Play/Pause</span>
-      </button>
+      <button onClick={handlePlayClick}>Play</button>
+      <button>Stop</button>
     </div>
   );
 }
